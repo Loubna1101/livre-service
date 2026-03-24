924f9901bd965dd7edd9e4f28e1c605b2c729b57
louna <haouach.loubnaaa@gmail.com>
2026-03-24 16:18:52 +0100

    livreservice

---
+++ src/main/java/com/india/biblio/livre_service/Livre.java
@@ -0,0 +1,24 @@
+package com.india.biblio.livre_service;
+
+public class Livre {
+	private int id;
+	private String title;
+	public Livre(int id, String title) {
+		super();
+		this.id = id;
+		this.title = title;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+}
